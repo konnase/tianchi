62a7d210919b420e7f7691bbc4d96daeaaba389b
Ying ZHANG <yingz_nju@outlook.com>
2018-07-31 08:49:37 +0800

    unsolved ffd degration

--- code/scheduler/ffd.py
+++ code/scheduler/ffd.py
@@ -6,7 +6,7 @@ import numpy as np
 from config import *
 from scheduler.models import Machine
 
-
+# todo: 重构过的ffd执行结果比之前版本变差很多
 class FFD(object):
     def __init__(self, instances, apps, machines):
         self.instances = instances
@@ -18,8 +18,12 @@ class FFD(object):
 
         self.submit_result = []
 
-    def first_fit(self, inst, machines):
+        # print machines[0].CPU_UTIL, machines[3001].CPU_UTIL
+
+    def first_fit(self, inst, machines, large_machine=False):
         for m in machines:
+            if large_machine and not m.is_large_machine:
+                continue
             if m.can_put_inst(inst):
                 m.put_inst(inst)
                 self.submit_result.append((inst.id, m.id))
@@ -37,16 +41,14 @@ class FFD(object):
     def fit_large_inst(self, machines):
         print "starting fit_large_inst"
         for app in self.apps:
-            if app.disk < LARGE_DISK_INST \
-                    and np.all(app.cpu < LARGE_CPU_INST) \
-                    and np.all(app.mem < LARGE_MEM_INST):
-                continue
-
-            for inst in app.instances:
-                if not inst.deployed:
-                    self.first_fit(inst, machines)
+            if app.disk > LARGE_DISK_INST \
+                    or np.any(app.cpu > LARGE_CPU_INST) \
+                    or np.any(app.mem > LARGE_MEM_INST):
+                for inst in app.instances:
+                    self.first_fit(inst, machines, large_machine=True)
 
     def fit(self):
+        # print Machine.total_score(self.machines)
         self.resolve_init_conflict(self.machines)
         self.fit_large_inst(self.machines)
         print "starting fit"
@@ -56,12 +58,14 @@ class FFD(object):
                     self.first_fit(inst, self.machines)
 
     def write_to_csv(self):
-        with open("submit.csv", "w") as f:
-            print "writing to submit.csv"
+        total_score = Machine.total_score(self.machines)
+        s = ("%.2f" % total_score).replace(".", "_")
+        csv = "submit%s.csv" % s
+        print "writing to", csv
+        with open(csv, "w") as f:
             for inst_id, machine_id in self.submit_result:
                 f.write("{0},{1}\n".format(inst_id, machine_id))
 
-        total_score = Machine.total_score(self.machines)
         machine_cnt = len(filter(lambda x: x.disk_usage > 0, self.machines))
 
         path = "search-result/search_%d_%dm" % (int(math.ceil(total_score)), machine_cnt)
